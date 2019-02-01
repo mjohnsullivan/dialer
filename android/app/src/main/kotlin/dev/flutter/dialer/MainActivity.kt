95ce6c228fb0f1ab505d1e3fda35799977ecdcd6
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-02-01 14:43:35 -0800

    Added permissions check and tweaked styling

--- android/app/src/main/kotlin/dev/flutter/dialer/MainActivity.kt
+++ android/app/src/main/kotlin/dev/flutter/dialer/MainActivity.kt
@@ -4,18 +4,26 @@
 
 package dev.flutter.dialer
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugin.common.MethodChannel
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.makeCall
+import org.jetbrains.anko.noButton
+import org.jetbrains.anko.yesButton
 
 
 class MainActivity: FlutterActivity() {
   private val channel = "dev.flutter.dialer/dialer"
+  private val permissionsRequestCode = 11011
+  private var number: String? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -24,7 +32,12 @@ class MainActivity: FlutterActivity() {
     MethodChannel(flutterView, channel).setMethodCallHandler { call, result ->
       print("Phone dialer called in Kotlin")
       when (call.method) {
-        "dial" -> result.success(callPhoneNumber(call.arguments as String))
+        "dial" -> {
+          val number = call.argument<String>("number")
+          number?.let {
+            callPhoneNumber(number)
+          }
+        }
         else -> result.notImplemented()
       }
     }
@@ -34,12 +47,75 @@ class MainActivity: FlutterActivity() {
    * Fires an intent to start a phone call
    * TODO: This should check for appropriate permission and fail gracefully
    */
-  @SuppressLint("MissingPermission")
-  fun callPhoneNumber(number: String) {
+  private fun callPhoneNumber(number: String) {
     // If you're using Anko, just use 'makeCall(number)'
-
-    val intent = Intent(Intent.ACTION_CALL)
-    intent.data = Uri.parse("tel:$number")
-    startActivity(intent)
+    this.number = number
+    if (isPermissionGranted(Manifest.permission.CALL_PHONE)) {
+      makeCall(number)
+    } else {
+      showPermissionReasonAndRequest(
+              "Dialer",
+              "Hello there. We need your permission to make a phone call. " +
+                      "If this is what you'd like to do, " +
+                      "please grant it.",
+              Manifest.permission.CALL_PHONE,
+              this.permissionsRequestCode
+      )
+    }
   }
+
+  /*
+   * Request permission to make a phone call
+   */
+  private fun Activity.showPermissionReasonAndRequest(
+          title: String,
+          message: String,
+          permission: String,
+          requestCode: Int) {
+    alert(
+            message,
+            title
+    ) {
+      yesButton {
+        ActivityCompat.requestPermissions(
+                this@showPermissionReasonAndRequest,
+                arrayOf(permission),
+                requestCode
+        )
+      }
+      noButton { }
+    }.show()
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int,
+                                          permissions: Array<String>,
+                                          grantResults: IntArray) {
+    when (requestCode) {
+      permissionsRequestCode -> {
+        // If request is cancelled, the result arrays are empty.
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+          number?.let {
+            makeCall(it)
+          }
+        } else {
+          // permission denied, boo! Disable the
+          // functionality that depends on this permission.
+        }
+        return
+      }
+
+      // Add other 'when' lines to check for other
+      // permissions this app might request.
+      else -> {
+        // Ignore all other requests.
+      }
+    }
+  }
+
+  private fun isPermissionGranted(permission:String):Boolean =
+          ContextCompat.checkSelfPermission(
+                  this,
+                  permission
+          ) == PackageManager.PERMISSION_GRANTED
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ import 'package:flutter/services.dart';
 
 import 'package:scoped_model/scoped_model.dart';
 
+final digitTextStyle = TextStyle(fontSize: 30);
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,53 +19,65 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Dialer',
       home: Scaffold(
           body: SafeArea(
-        child: ScopedModel<PhoneNumberModel>(
-          model: PhoneNumberModel(),
-          child: Dialer(),
-        ),
+        child: Dialer(),
       )),
     );
   }
 }
 
-class Dialer extends StatelessWidget {
+/// Dialer is a stateful widget. It holds the phone number model
+/// which will preserve the model state between hot reloads
+class Dialer extends StatefulWidget {
+  @override
+  createState() => _DialerState();
+}
+
+class _DialerState extends State<Dialer> {
+  final model = PhoneNumberModel();
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      NumText(),
-      NumPad(),
-      SizedBox(height: 50),
-      DialButton(),
-    ]);
+    return ScopedModel<PhoneNumberModel>(
+      model: model,
+      child: Column(children: <Widget>[
+        NumberReadout(),
+        NumberPad(),
+        SizedBox(height: 50),
+        DialButton(),
+      ]),
+    );
   }
 }
 
-class NumPad extends StatelessWidget {
+class NumberPad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Table(
-      children: <TableRow>[
-        TableRow(
-          children: ['1', '2', '3']
-              .map<Widget>((char) => DigitButton(char: char))
-              .toList(),
-        ),
-        TableRow(
-          children: ['4', '5', '6']
-              .map<Widget>((char) => DigitButton(char: char))
-              .toList(),
-        ),
-        TableRow(
-          children: ['7', '8', '9']
-              .map<Widget>((char) => DigitButton(char: char))
-              .toList(),
-        ),
-        TableRow(
-          children: ['*', '0', '#']
-              .map<Widget>((char) => DigitButton(char: char))
-              .toList(),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Table(
+        children: <TableRow>[
+          TableRow(
+            children: ['1', '2', '3']
+                .map<Widget>((char) => DigitButton(char: char))
+                .toList(),
+          ),
+          TableRow(
+            children: ['4', '5', '6']
+                .map<Widget>((char) => DigitButton(char: char))
+                .toList(),
+          ),
+          TableRow(
+            children: ['7', '8', '9']
+                .map<Widget>((char) => DigitButton(char: char))
+                .toList(),
+          ),
+          TableRow(
+            children: ['*', '0', '#']
+                .map<Widget>((char) => DigitButton(char: char))
+                .toList(),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -81,11 +95,50 @@ class DigitButton extends StatelessWidget {
         builder: (context, _, model) => RawMaterialButton(
               shape: CircleBorder(),
               elevation: 6,
-              fillColor: Colors.grey,
-              padding: const EdgeInsets.all(20),
-              child: Text(char),
+              fillColor: Colors.white,
+              padding: const EdgeInsets.all(10),
+              child: Text(char, style: digitTextStyle),
               onPressed: () => model.addDigit(char),
             ),
+      ),
+    );
+  }
+}
+
+/// Displays the entered phone number
+class NumberReadout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: ScopedModelDescendant<PhoneNumberModel>(
+              builder: (context, _, model) {
+                return Text(
+                  model.number,
+                  textAlign: TextAlign.center,
+                  style: digitTextStyle,
+                );
+              },
+            ),
+          ),
+          ScopedModelDescendant<PhoneNumberModel>(
+            rebuildOnChange: false,
+            builder: (context, _, model) {
+              return IconButton(
+                icon: Icon(Icons.backspace),
+                onPressed: () => model.removeDigit(),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
@@ -95,14 +148,17 @@ class DigitButton extends StatelessWidget {
 class DialButton extends StatelessWidget {
   static const platform = MethodChannel('dev.flutter.dialer/dialer');
 
-  Future<void> _dialNumber(String phoneNumber) async {
+  Future<void> _dialNumber(String phoneNumber, BuildContext context) async {
     assert(phoneNumber != null && phoneNumber.isNotEmpty);
-
     try {
-      print('Inovking remote');
-      await platform.invokeMethod('dial', phoneNumber);
+      print('Invoking remote');
+      await platform.invokeMethod('dial', <String, String>{
+        'number': phoneNumber,
+      });
     } on PlatformException catch (e) {
-      print('Unable to dial number *number*: error $e');
+      Scaffold.of(context).showSnackBar(
+        SnackBar(content: Text('Unable to dial $phoneNumber: error $e')),
+      );
     }
   }
 
@@ -111,24 +167,10 @@ class DialButton extends StatelessWidget {
     return ScopedModelDescendant<PhoneNumberModel>(
       builder: (context, _, model) => FloatingActionButton(
             child: Icon(Icons.phone),
-            onPressed: () =>
-                model._number.isNotEmpty ? _dialNumber(model._number) : null,
+            onPressed: () => model._number.isNotEmpty
+                ? _dialNumber(model._number, context)
+                : null,
           ),
-    );
-  }
-}
-
-/// Displays the entered phone number
-class NumText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: ScopedModelDescendant<PhoneNumberModel>(
-        builder: (context, _, model) {
-          return Text(model.number);
-        },
-      ),
     );
   }
 }
@@ -138,10 +180,34 @@ class PhoneNumberModel extends Model {
   /// Represented as a string to handle chars like #, *
   var _number = '';
 
-  String get number => _number;
+  String get number => _formatPhoneNumber(_number);
 
   void addDigit(String digit) {
     _number += digit;
     notifyListeners();
+  }
+
+  void removeDigit() {
+    if (_number.length > 0) {
+      _number = _number.substring(0, _number.length - 1);
+      notifyListeners();
+    }
+  }
+}
+
+String _formatPhoneNumber(String number) {
+  switch (number.length) {
+    case (0):
+    case (1):
+    case (2):
+    case (3):
+      return number;
+    case (4):
+    case (5):
+    case (6):
+    case (7):
+      return ('${number.substring(0, 3)}-${number.substring(3)}');
+    default:
+      return ('(${number.substring(0, 3)}) ${number.substring(3, 6)}-${number.substring(6)}');
   }
 }
