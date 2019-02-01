3ff24897ca39c574dd0f1562fdeaa740e99e2e38
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-02-01 10:24:27 -0800

    Tweaks to Kotlin code

--- android/app/src/main/kotlin/dev/flutter/dialer/MainActivity.kt
+++ android/app/src/main/kotlin/dev/flutter/dialer/MainActivity.kt
@@ -1,3 +1,7 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package dev.flutter.dialer
 
 import android.annotation.SuppressLint
@@ -8,17 +12,16 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.Result
 
 
 class MainActivity: FlutterActivity() {
-  private val CHANNEL = "dev.flutter.dialer/dialer"
+  private val channel = "dev.flutter.dialer/dialer"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
 
-    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { call, result ->
+    MethodChannel(flutterView, channel).setMethodCallHandler { call, result ->
       print("Phone dialer called in Kotlin")
       when (call.method) {
         "dial" -> result.success(callPhoneNumber(call.arguments as String))
