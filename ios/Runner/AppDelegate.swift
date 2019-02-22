a573be32c600e16440c6bb2d04dc89ed30678d53
Matt Sullivan <matt.j.sullivan@gmail.com>
2019-02-22 22:53:51 +0100

    Updated dialer to work on iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,33 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Dailer specific code
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let dialerChannel = FlutterMethodChannel(name: "dev.flutter.dialer/dialer",
+                                              binaryMessenger: controller)
+    dialerChannel.setMethodCallHandler({
+        (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        let dict = call.arguments as? Dictionary<String, String>
+        if let unwrappedDict = dict {
+            let numberStr = unwrappedDict["number"]
+            if let unwrappedStr = numberStr {
+                makeCall(number: unwrappedStr, result: result)
+            }
+        }
+    })
+    
+    // End of dialer specific code
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+private func makeCall(number: String, result: FlutterResult) {
+    guard let urlNumber = URL(string: "tel://" + number) else { return }
+    // UIApplication.shared.open(urlNumber)
+    UIApplication.shared.openURL(urlNumber)
+    result(0)
 }
